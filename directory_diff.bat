b83535a7145e79e853b4b037970f8ae53913baa0
H/S <kidoairakusyunkasyuutou@gmail.com>
2016-03-06 08:33:58 +0900

    WinMerge

---
+++ directory_diff.bat
@@ -0,0 +1,16 @@
+﻿@echo off
+
+if not "%1"=="" (
+  SET PARAM1=%1
+)
+if not "%2"=="" (
+  SET PARAM2=%2
+) else (
+  SET PARAM2=HEAD
+)
+
+if "%1"=="" (
+  echo ERROR:Need parameter!
+)
+
+git difftool --dir-diff %PARAM1% %PARAM2%
